b9a474245aae1696d890c3c55e0015dd355d0c04
Emman <jlr0020@alu.medac.es>
2025-09-23 17:48:43 +0200

    ajedrez

--- TEMA 1/Ejercicios DES/validarpass.php
+++ TEMA 1/Ejercicios DES/validarpass.php
@@ -1,19 +1,35 @@
 <?php
 
-function validarPass(String $pass):bool
+function validarPass(string $pass): bool
 {
-if (strlen($pass) < 8 ) return false;
-$tieneMayus = (bool)preg_match("/[A-Z]/" , $pass);
-$tieneMinus = (bool)preg_match("/[a-z]/" , $pass);
-$tieneDig = (bool)preg_match("/\d/" , $pass);
-return $tieneMayus && $tieneMinus && $tieneDig;
-}
- $p = "JBCSBUbdjbb894acnc";
+    if (strlen($pass) < 8) {
+        echo "\n La contraseña debe tener al menos 8 caracteres";
+        return false;
+    }
 
- if(validarPass(pass: $p)){
-    echo"Contrasena segura";
- }else{
-    echo"Contrasena no segura";
- }
+    $tieneMayus = (bool)preg_match("/[A-Z]/", $pass);
+    $tieneMinus = (bool)preg_match("/[a-z]/", $pass);
+    $tieneDig   = (bool)preg_match("/\d/", $pass);
+
+    if (!$tieneMayus) {
+        echo "\n Falta letra mayúscula";
+    }
+    if (!$tieneMinus) {
+        echo "\n Falta letra minúscula";
+    }
+    if (!$tieneDig) {
+        echo "\n Falta un digito";
+    }
+
+    return $tieneMayus && $tieneMinus && $tieneDig;
+}
+
+$p = "Contrasena1234";
+
+if (validarPass($p)) {
+    echo "\nContraseña segura";
+} else {
+    echo "\nContraseña no segura";
+}
 
 ?>
