29178ce4444aaa33c7dd64647f2d5575bfe38bfc
Emman <jlr0020@alu.medac.es>
2025-09-23 17:54:05 +0200

    actual

--- TEMA 1/ajedrez.php
+++ TEMA 1/ajedrez.php
@@ -5,9 +5,9 @@ for ($i = 0; $i < 5; $i++ ){
     for($j = 0; $j < 5 ; $j++){
 
         if(($i + $j) % 2 == 0 ){
-            echo " N ";
+            echo " ⬛ ";
         }else{
-            echo " B ";
+            echo " ⬜ ";
         }
 
     }echo "\n";
