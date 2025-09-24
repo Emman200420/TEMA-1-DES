503a3bdce19ce87e565dba8e8256ae611d1cbe15
Emman <jlr0020@alu.medac.es>
2025-09-24 15:26:27 +0200

    ajedrez

--- TEMA 1/ajedrez.php
+++ TEMA 1/ajedrez.php
@@ -1,13 +1,13 @@
 <?php
 
-for ($i = 0; $i < 5; $i++ ){
+for ($i = 0; $i < 10; $i++ ){
 
-    for($j = 0; $j < 5 ; $j++){
+    for($j = 0; $j < 10 ; $j++){
 
         if(($i + $j) % 2 == 0 ){
-            echo " ⬛ ";
+            echo "⬛";
         }else{
-            echo " ⬜ ";
+            echo "⬜";
         }
 
     }echo "\n";
