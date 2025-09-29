39c3950425006549cdfb6930f26542149daaa119
Emman <jlr0020@alu.medac.es>
2025-09-29 19:38:44 +0200

    actualizacion

---
+++ TEMA 1/alumno aleatorio.php
@@ -0,0 +1,6 @@
+<?php
+$alumnos = ["miriam","patricia","raul","Carlos PRO","javi","shail","ruben","Sebastian","Carlos S","Jorge","AKO","Joel","Felix","aitor"];
+
+$numaleatorio = rand(0,13);
+echo $alumnos[$numaleatorio];
+?>
