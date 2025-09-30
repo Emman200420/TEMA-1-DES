9845ba1ea97b64860b354c86b75912bb52fb2ace
Emman <jlr0020@alu.medac.es>
2025-09-30 15:14:20 +0200

    actualizacion

---
+++ TEMA 1/recursividad.php
@@ -0,0 +1,58 @@
+<?php
+/* La recursividad es una tecnica donde una funcion se  llama a si misma,par resolver e problema atomizado.
+
+Dos piezas claves:
+    -Caso base.
+    -Caso recursivo.
+ */
+$num = 7;
+
+Parafactorial($num);
+function Parafactorial($num){
+
+    if($num == 1)
+        return 1;
+    else 
+        return $num * Parafactorial($num - 1);
+
+}
+echo "\n La factorial sera " . Parafactorial($num);
+
+
+
+
+
+
+function fivonachi($num){
+    if($num == 1)
+        return 1;
+    else if ($num == 0)
+        return 0;
+    else 
+        return fivonachi($num - 1) + fivonachi($num - 2);
+}
+
+echo "\n la fivonachi es " . fivonachi($num);
+
+
+
+
+$n = 7;
+
+function cuentaregresiva($n){
+
+    if ($n == 1){
+        echo'1';
+
+    }else {
+        echo $n . "\n";
+        cuentaregresiva($n-1);
+
+    }
+}
+
+cuentaregresiva($n);
+
+
+
+?>
